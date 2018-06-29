18f002169757c7676d5389e845c14bd4ffc55320
Jasmine QIAN <jasqia@acxiom.com>
2018-06-29 17:25:42 +0800

    first commit

---
+++ src/main/java/DesignModule/Apple.java
@@ -0,0 +1,23 @@
+package DesignModule;
+
+public class Apple {
+
+    private  int a;
+    private  String color;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
